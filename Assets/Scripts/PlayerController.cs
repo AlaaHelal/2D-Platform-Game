c7eb278a7b8a33b8c5a0e833789e67a9a15dabfe
AlaaAllahHelal <alaa.helal98@gmail.com>
2025-10-23 20:28:32 +0300

    Animation & Double Jumpin
    -Set the animation
    -Update the double Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,31 +6,89 @@ public class PlayerController : MonoBehaviour
     private float jumpForce = 10.0f;
     private float groundCheckRadius = 0.2f;
     private bool isGrounded;
+    public int extraJumpsCount =1;
+    private int extraJumpsLeft;
 
     public Transform groundCheck;
     public LayerMask groundLayer;
     private Rigidbody2D rbPlayer;
+    private Animator animator;
 
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        // Initialize extra jumps
+        extraJumpsLeft = extraJumpsCount;
     }
 
     
     void Update()
     {
+        // Handle horizontal movement
         float horizontalInput = Input.GetAxis("Horizontal");
         rbPlayer.linearVelocity = new Vector2(horizontalInput * moveSpeed, rbPlayer.linearVelocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        // Ground Check and Reset Extra Jumps Left
+        if (isGrounded)
         {
-            rbPlayer.linearVelocity =new Vector2(rbPlayer.linearVelocity.x, jumpForce);
+            extraJumpsLeft = extraJumpsCount;
         }
+
+        //Double Jump Logic
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded || extraJumpsLeft > 0)
+            {
+                Debug.Log($"{extraJumpsLeft}");
+                rbPlayer.linearVelocity = new Vector2(rbPlayer.linearVelocity.x, jumpForce);
+                Debug.Log($"{extraJumpsLeft}");
+                extraJumpsLeft--;
+                Debug.Log($"{extraJumpsLeft}");
+            }
+        }
+        
+            
+
+        // Update animations
+        SetAnimations(horizontalInput);
     }
 
 
     void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    public void SetAnimations(float moveInput)
+    {
+        if (isGrounded)
+        {
+            if(moveInput == 0)
+            {
+                // Set idle animation
+                animator.Play("Player_Idle");
+            }
+            else
+            {
+                // Set running animation
+                animator.Play("Player_Run");
+            }
+        }
+        else
+        {
+            if(rbPlayer.linearVelocityY > 0)
+                {
+                // Set jumping animation
+                animator.Play("Player_Jump");
+            }
+            else
+            {
+                // Set falling animation
+                animator.Play("Player_Fall");
+            }
+        }
+
     }
 }
